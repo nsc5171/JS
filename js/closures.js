f25017c953c7c03585a05086530b40f6e02bcccd
nsc5171 <nsaichandra55@gmail.com>
2018-10-25 23:50:28 +0530

    closures and loops

--- js/closures.js
+++ js/closures.js
@@ -1,13 +1,36 @@
 for (var i=1; i<=5; i++) {
-	setTimeout( function timer(){
+    setTimeout( function timer(){
 		console.log( i );
 	}, i*1000 );
 }
 
-for (var i=1; i<=5; i++) {
+/* for (var i=1; i<=5; i++) {
 	(function(j){
 		setTimeout( function timer(){
 			console.log( j );
 		}, j*1000 );
 	})( i );
-}
+} */
+
+
+/* for (var i=1; i<=5; i++) {
+    var j=i;
+	setTimeout( function timer(){
+		console.log( j );
+	}, i*1000 );
+} */
+
+/* for (var i=1; i<=5; i++) {
+    let j=i;
+	setTimeout( function timer(){
+		console.log( j );
+	}, i*1000 );
+} */
+
+/* for (var i=1; i<=5; i++) {
+    var j=i;
+    let a=i;
+	setTimeout( function timer(){
+		console.log( j + " " + a );
+	}, i*1000 );
+} */
