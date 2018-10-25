cf016c32b719cd81d7ef253473ef24e6d807eef3
nsc5171 <nsaichandra55@gmail.com>
2018-10-25 23:39:51 +0530

    closures

--- js/closures.js
+++ js/closures.js
@@ -3,3 +3,11 @@ for (var i=1; i<=5; i++) {
 		console.log( i );
 	}, i*1000 );
 }
+
+for (var i=1; i<=5; i++) {
+	(function(j){
+		setTimeout( function timer(){
+			console.log( j );
+		}, j*1000 );
+	})( i );
+}
